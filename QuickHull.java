01802609b4eef13d76fbca9312175bbce7634458
hjbossi <hjbossi@colby.edu>
2017-04-05 23:41:43 -0400

    Add files via upload

--- QuickHull.java
+++ QuickHull.java
@@ -5,80 +5,139 @@ import java.lang.Math;
 
 public class QuickHull {
 
-	private ArrayList<Point> input;
-	private int n;
-	private ArrayList<Point> output;
+    private ArrayList<Point> input;
+    private int n;
+    private ArrayList<Point> output;
 	
-	public QuickHull( ArrayList<Point> S ){
-		//creates a QuickHull object from the given ArrayList of points
-		input = S;
-		output = new ArrayList<Point>();
-		n = S.size();
-		System.out.println( this.input );
+    public QuickHull( ArrayList<Point> S ){
+	//creates a QuickHull object from the given ArrayList of points
+	input = S;
+	output = new ArrayList<Point>();
+	n = S.size();
+	System.out.println( this.input );
+    }
+	
+    // performs the max/min calculation
+    // to be used later in recursion
+    public Point[] getExtremes( ArrayList<Point> a ) {
+	Point[] output = new Point[2];
+	Point min = a.get(0);
+	Point max = a.get(0);
+	for( int i = 0; i < n; i++ ) {
+	    Point p = a.get(i);
+	    if( p.getX() < min.getX() ) {
+		min = p;
+	    }
+	    if( p.getX() > max.getX() ) {
+		max = p;
+	    }
 	}
+	output[0] = min;
+	output[1] = max;
+	return output;
+    }
 	
-	// performs the max/min calculation
-	// to be used later in recursion
-	public Point[] getExtremes( ArrayList<Point> a ) {
-		Point[] output = new Point[2];
-		Point min = a.get(0);
-		Point max = a.get(0);
-		for( int i = 0; i < n; i++ ) {
-			Point p = a.get(i);
-			if( p.getX() < min.getX() ) {
-				min = p;
-			}
-			if( p.getX() > max.getX() ) {
-				max = p;
-			}
-		}
-		output[0] = min;
-		output[1] = max;
-		return output;
-	}
 	
-	public ArrayList<Point> getConvexHull() {
+    // Function that actually finds the convex hull
+    public ArrayList<Point> getConvexHull() {
 		
-		Point[] result = this.getExtremes(input);
-		Point min = result[0];
-		Point max = result[1];
+	Point[] result = this.getExtremes(input);
+	Point min = result[0];
+	Point max = result[1];
 
-		//System.out.println( "min: " + String.valueOf(min) + "	" + 
-							//"max: " + String.valueOf(max) );
+	//System.out.println( "min: " + String.valueOf(min) + "	" + 
+	//"max: " + String.valueOf(max) );
 		
-		Segment s = new Segment( min,max );
+	Segment s = new Segment( min,max );
 		
-		// extremes in x will be a part of the convex hull
-		output.add( min );
-		output.add( max );
-		
-		// get all of the points on the outward portion of the line
-		
-		// perform the recursive algorithm on the two points
+	// extremes in x will be a part of the convex hull
+	this.output.add( min );
+	this.output.add( max );
 
-		return output;
-	}
-	
-	public void getPoints( ArrayList<Point> s, Point min, Point max ) {
+	ArrayList<Point> left = new ArrayList<Point>();
+	ArrayList<Point> right = new ArrayList<Point>(); 
+
+	// get a list of all of the points to the left of the line
+	for(int i = 0; i< this.n; i++){
+	    Point p = this.input.get(i);
+	    if (s.isLeft(p) == true){
+		left.add(p);
+	    }
+	    else{
+		right.add(p); 
+	    }
+	}	
+	// perform the recursive algorithm on the two points
+	this.subHull(left,s);
+	this.subHull(right,s); 
 		
+
+	return this.output;
+    }
+
+    // write the recursive algorithm.
+    public void subHull(ArrayList<Point> a, Segment s ) {
+
+	// base case for recursion
+	if (a.size() == 1){
+	    return; 
 	}
 	
-// 	public float distance( Segment s, Point p ) {
-// 		a,b,c = s.getCo();
-// 		dist = Math.abs( a*p.getX() + b*p.getY() + c )/Math.sqrt( a*a + b*b );
-// 		return dist;
-// 	}
+	//loop through the arrayList to find the point with the max distance
+	double max = Double.MIN_VALUE;
+	Point maxPoint = a.get(0);
 	
-	public static void main( String args[] ) {
-		ArrayList<Point> S = new ArrayList<Point>();
-		Random rand = new Random();
-		for( int i = 0; i < 10; i++) {
-			int x = rand.nextInt(100);
-			int y = rand.nextInt(100);
-			S.add( new Point( x, y ) );
-		}
+	//find the point with the max perpendicular distance
+	for(int i = 0; i< a.size(); i++){
+	    double dist = this.distance(s, a.get(i));
+	    if(dist>max){
+		max = dist; 
+		maxPoint = a.get(i); 
+	    }
+	}
+
+	// add the max point to the convex hull
+	this.output.add(maxPoint); 
+
+	//create a segment between that point and the max and min of the segment
+	Segment min2dist = new Segment(s.getP1(), maxPoint);
+	Segment max2dist = new Segment(s.getP2(), maxPoint);
+
+
+	ArrayList<Point> left = new ArrayList<Point>();
+
+	// get a list of all of the points to the left of the line
+	for(int i = 0; i< this.n; i++){
+	    Point p = this.input.get(i);
+	    if (s.isLeft(p) == true){
+		left.add(p);
+	    }
+       
+	}
+
+	// call algorithm recursively
+	subHull(left, min2dist);
+	subHull(left, max2dist); 
+
+	
+    }
+	
+    public double distance( Segment s, Point p ) {
+	double[] a = s.getCo();
+	double dist = Math.abs( a[0]*p.getX() + a[1]*p.getY() + a[2] )/Math.sqrt( a[0]*a[0] + a[1]*a[1] );
+	return dist;
+    }
+	
+    public static void main( String args[] ) {
+	ArrayList<Point> S = new ArrayList<Point>();
+	Random rand = new Random();
+	for( int i = 0; i < 10; i++) {
+	    int x = rand.nextInt(100);
+	    int y = rand.nextInt(100);
+	    S.add( new Point( x, y ) );
+	}
 		
-		QuickHull QH = new QuickHull( S );
-		QH.getConvexHull();
-	}
+	QuickHull QH = new QuickHull( S );
+	System.out.println(QH.getConvexHull());
+    }
 }
--- Segment.java
+++ Segment.java
@@ -6,52 +6,74 @@ import java.awt.Point;
 import java.lang.Math;
 
 public class Segment {
-	private Point p1;
-	private Point p2;
+    private Point p1;
+    private Point p2;
+    private double slope;
+    private double intercept;
+    
 
-	// constructor
-	public Segment(Point point1, Point point2) {
-		this.p1 = point1;
-		this.p2 = point2;
-	}
+    // constructor
+    public Segment(Point point1, Point point2) {
+	this.p1 = point1;
+	this.p2 = point2;
+	double xVal = this.p1.getX() - this.p2.getX();
+	double yVal = this.p1.getY() - this.p2.getY();
+	this.slope = (yVal/xVal);
+	this.intercept = this.p1.getY() - this.slope*this.p1.getX(); 
+    }
 
-	private double getLength() {
-		double xVal = Math.abs(this.p1.getX() - this.p2.getX());
-		double yVal = Math.abs(this.p1.getY() - this.p2.getY());
-		double length = Math.sqrt(Math.pow(xVal, 2)+Math.pow(yVal, 2));
-		return length;
-	}
+    private double getLength() {
+	double xVal = Math.abs(this.p1.getX() - this.p2.getX());
+	double yVal = Math.abs(this.p1.getY() - this.p2.getY());
+	double length = Math.sqrt(Math.pow(xVal, 2)+Math.pow(yVal, 2));
+	return length;
+    }
 
-	private double [] getCo() {
-		double [] Co = new double[3];
-		double A = this.p1.getY() - this.p2.getY();
-		double B = this.p2.getX() - this.p1.getX();
-		double C = (this.p1.getX()*this.p2.getY()) - (this.p2.getX()*this.p1.getY());
-		Co[0] = A;
-		Co[1] = B;
-		Co[2] = C;
-		return Co;
-	}
+    public double[] getCo() {
+	double [] Co = new double[3];
+	double A = this.p1.getY() - this.p2.getY();
+	double B = this.p2.getX() - this.p1.getX();
+	double C = (this.p1.getX()*this.p2.getY()) - (this.p2.getX()*this.p1.getY());
+	Co[0] = A;
+	Co[1] = B;
+	Co[2] = C;
+	return Co;
+    }
 
-	private Point getP1() {
-		return this.p1;
-	}
+    public Point getP1() {
+	return this.p1;
+    }
 
-	private Point getP2() {
-		return this.p2;
-	}
+    public Point getP2() {
+	return this.p2;
+    }
 
-	public static void main(String args[]) {
-		System.out.println("working");
-		Point p1 = new Point(2,1);
-		Point p2 = new Point(5,7);
-		Segment s1 = new Segment(p1, p2);
-		System.out.println("Point created");
-		double length1 = s1.getLength();
-		System.out.println(length1);
-		double[] Co = s1.getCo();
-		System.out.println(Co[0]);
-		System.out.println(Co[1]);
-		System.out.println(Co[2]);
+
+    
+    // function to calculate if a point is to the left of a line segment
+    public Boolean isLeft(Point p){
+	if (p.getY() >= this.slope*p.getX() + this.intercept){
+	    return true; 
 	}
+	else{
+	    return false; 
+	}
+	    
+		
+	
+	
+    }
+    public static void main(String args[]) {
+	System.out.println("working");
+	Point p1 = new Point(2,1);
+	Point p2 = new Point(5,7);
+	Segment s1 = new Segment(p1, p2);
+	System.out.println("Point created");
+	double length1 = s1.getLength();
+	System.out.println(length1);
+	double[] Co = s1.getCo();
+	System.out.println(Co[0]);
+	System.out.println(Co[1]);
+	System.out.println(Co[2]);
+    }
 }
