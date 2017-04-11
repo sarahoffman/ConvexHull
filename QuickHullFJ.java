b3599e615a962cd59913fde7976856fd1c19e5ef
rskarp <rskarp@colby.edu>
2017-04-10 21:59:55 -0400

    Add files via upload

--- QuickHullFJ.java
+++ QuickHullFJ.java
@@ -16,7 +16,7 @@ public class QuickHullFJ extends QuickHull {
 		super(in);
 		input = in;
 		numProc = Runtime.getRuntime().availableProcessors();
-		System.out.println( "Number of processors: " + numProc );
+// 		System.out.println( "Number of processors: " + numProc );
 		pool = new ForkJoinPool();
 	}
 	
@@ -47,7 +47,6 @@ public class QuickHullFJ extends QuickHull {
 			}	
 			pool.invoke(new QuickHullAction(left, s, output));
 			pool.invoke(new QuickHullAction(right, s, output));
-			System.out.println( "ASYNC: " + pool.getAsyncMode() );
 		}
         return output;
     }
@@ -61,21 +60,15 @@ public class QuickHullFJ extends QuickHull {
             in = a;
             seg = s;
             output = r;
-            System.out.println( "\nSegment endpoints: " + seg.getP1() + " ' " + seg.getP2() );
-            System.out.println( "Contents of in: " + in );
+//             System.out.println( "\nSegment endpoints: " + seg.getP1() + " ' " + seg.getP2() );
+//             System.out.println( "Contents of in: " + in );
         }
 
         @Override
         protected void compute() {
-            if ( pool.getPoolSize() >= numProc || in.size() < threshold ) { // do it sequentially myself
+            if ( in.size() < threshold ) { // do it sequentially myself
             	subHull( in , seg );
             }
-//             else if ( pool.getStealCount() == getSurplusQueuedTaskCount() && 
-//             		getSurplusQueuedTaskCount() > 0 ) {
-//             		//getSurplusQueuedTaskCount() == 0 also pool.getStealCount() > 1
-//             	System.out.println( "\nSURPLUS TASK COUNT: " + getSurplusQueuedTaskCount() + "\n");
-//             	pool.shutdown();
-//             }
             else { // fork the work into two tasks for other threads
 				//loop through the arrayList to find the point with the max distance
 				double max = Double.MIN_VALUE;
@@ -89,7 +82,7 @@ public class QuickHullFJ extends QuickHull {
 					}
 				}
 				
-				System.out.println( "maxPoint: " + maxPoint );
+// 				System.out.println( "maxPoint: " + maxPoint );
 
 				// add the max point to the convex hull
 				if (!output.contains(maxPoint)) {
@@ -110,14 +103,14 @@ public class QuickHullFJ extends QuickHull {
 				// this is due to our isLeft method
 		
 				// is maxPoint is negative
-				if (0 >= maxPoint.getY() || seg.getP2().getY() >= maxPoint.getY()){
+				if (seg.getP2().getY() >= maxPoint.getY()){
 					for(int i = 0; i< in.size(); i++){
 						Point p = in.get(i);
 						if (seg2.isLeft(p) == true){
-							left.add(p);
+							right.add(p);
 						} 
 						else if (seg1.isLeft(p) == false) {
-							right.add(p);
+							left.add(p);
 						}
 					}
 				}
@@ -139,9 +132,7 @@ public class QuickHullFJ extends QuickHull {
             
                 QuickHullAction l = new QuickHullAction(left, seg1, output);
                 QuickHullAction r = new QuickHullAction(right, seg2, output);
-//                 invokeAll(l, r);
-                l.fork();
-                r.fork();
+                invokeAll(l, r);
             }
         }
     }
@@ -149,33 +140,11 @@ public class QuickHullFJ extends QuickHull {
     public static void main( String args[] )  {
     	Random rand = new Random();
     	ArrayList<Point> in = new ArrayList<Point>();
-//     	for( int i = 0; i < 10; i++) {
-// 	        int x = rand.nextInt(100);
-// 			int y = rand.nextInt(100)-50;
-// 	        	in.add( new Point( x, y ) );
-// 		}
-
-// 		in.add( new Point( 15,-3 ) );
-// 		in.add( new Point( 43, -14 ) );
-// 		in.add( new Point( 1, 29 ) );
-// 		in.add( new Point( 79, 4 ) );
-// 		in.add( new Point( 67, -20 ) );
-// 		in.add( new Point( 20, -23 ) );
-// 		in.add( new Point( 7, -27 ) );
-// 		in.add( new Point( 49, 47 ) );
-// 		in.add( new Point( 24, 23 ) );
-// 		in.add( new Point( 47, 7 ) );
-
-		in.add( new Point( 43,46 ) );
-		in.add( new Point( 56,39 ) );
-		in.add( new Point( 48,-38 ) );
-		in.add( new Point( 93,-20 ) );
-		in.add( new Point( 25,0 ) );
-		in.add( new Point( 84, -24 ) );
-		in.add( new Point( 20,20 ) );
-		in.add( new Point( 60,-32 ) );
-		in.add( new Point( 66,28 ) );
-		in.add( new Point( 85,-34 ) );
+    	for( int i = 0; i < 10; i++) {
+	        int x = rand.nextInt(100);
+			int y = rand.nextInt(100)-50;
+	        	in.add( new Point( x, y ) );
+		}
 
 // 		in.add( new Point( 0, 4 ) );
 // 		in.add( new Point( 0, 0 ) );
@@ -183,9 +152,13 @@ public class QuickHullFJ extends QuickHull {
 // 		in.add( new Point( 4,4 ) );
 // 		in.add( new Point( 2, 2 ) );
 		
-		System.out.println( "in: " + in );
+		System.out.println( "input: " + in );
+		
 		QuickHullFJ qhfj = new QuickHullFJ( in );
+		long startTime = System.nanoTime();
 		ArrayList<Point> output = qhfj.findConvHull();
-		System.out.println( "Output: " + output );
+		long endTime = System.nanoTime();
+		System.out.println( "\nOutput: " + output + "Size: " + output.size() );
+		System.out.println( "\nTime: " + (endTime - startTime) / 1000000 );
     }
 }
